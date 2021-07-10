542237e23901d724191347bbc3f30b56dd45767c
bylands <samuel@byland.name>
2021-07-10 19:50:15 +0200

    Included DataFrame

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print('Hello world!')
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        'Name': [
+            'Samuel Byland',
+            'Katrin Leenen',
+            'Marlon Byland'
+        ],
+        'Age': [
+            50,
+            48,
+            18
+        ],
+        'Sex': [
+            'm',
+            'f',
+            'm'
+        ]
+    }
+)
+print(df)
