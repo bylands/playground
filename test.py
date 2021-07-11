818496392e72ff90fbe88752e667b787f22709fa
bylands <samuel@byland.name>
2021-07-11 14:40:37 +0200

    Included statistics on class

--- test.py
+++ test.py
@@ -1,22 +1,19 @@
+import numpy as np
 import pandas as pd
+import matplotlib
 
-df = pd.DataFrame(
-    {
-        'Name': [
-            'Samuel Byland',
-            'Katrin Leenen',
-            'Marlon Byland'
-        ],
-        'Age': [
-            50,
-            48,
-            18
-        ],
-        'Sex': [
-            'm',
-            'f',
-            'm'
-        ]
-    }
-)
-print(df)
+classes = pd.read_excel('data/klassen_21-22.xlsx')
+classes = classes.drop(classes.index[-7:])
+classes = classes.drop(['Spalte1'], axis=1)
+classes['Imm'].fillna('nein', inplace=True)
+classes['Wechsel SPF'].fillna('', inplace=True)
+classes['Bemerkungen'].fillna('', inplace=True)
+classes['ZZ'].fillna('', inplace=True)
+
+
+
+print('höchster Notenschnitt: ', classes['MW'].max())
+print('tiefster Notenschnitt: ', classes['MW'].min())
+print('mittlerer Notenschnitt: ', classes['MW'].mean())
+
+classes['MW'].plot.hist(bins=10)
